09f460a7c081f9deb6efc769119fda36ae4b494e
lucasucal <lucas.limzr@gmail.com>
2025-10-16 19:58:36 +0100

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -7,8 +7,8 @@ export interface Event {
   // Add other fields as needed
 }
 
-const supabaseUrl: string = import.meta.env.VITE_SUPABASE_URL as string;
-const supabaseAnonKey: string = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+const supabaseUrl: string =  "https://vascgynbztwepxpenkiq.supabase.co/" as string;
+const supabaseAnonKey: string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhc2NneW5ienR3ZXB4cGVua2lxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA0Njg3OTMsImV4cCI6MjA3NjA0NDc5M30.263x7iXLIi0unSsM0rPaJX4OYe8H6mVf-U3Hj9bGD0k" as string;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
