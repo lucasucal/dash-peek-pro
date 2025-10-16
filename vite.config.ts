1220839654bf4ae95a946121f26f00fcf84567e7
Tan Wee Joe <84664178+w3joe@users.noreply.github.com>
2025-10-16 19:26:19 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,27 +15,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, "index.html"),       // your React app entry
-        tracker: path.resolve(__dirname, "src/tracker.ts") // standalone tracker
-      },
-      output: {
-        // keep names separate for multiple outputs
-        entryFileNames: (chunk) => chunk.name === "tracker" ? "tracker.js" : "[name].js",
-      }
-    },
-    outDir: "public", // output to public so it can be loaded externally
-    emptyOutDir: false, // don't delete React build output
-    lib: {
-      entry: path.resolve(__dirname, "src/tracker.js"),
-      name: "OnflowTracker",
-      formats: ["iife"], // standalone script for embedding
-      fileName: "tracker",
-    },
-  },
-  define: {
-    "process.env": process.env, // if needed
-  },
 }));
